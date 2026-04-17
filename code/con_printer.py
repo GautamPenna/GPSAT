995590778f7158c03cd48e38ac7f0810fb77b450
Gautam Penna <gautam.penna@gmail.com>
2026-04-17 11:45:59 -0500

    Delete code directory

--- code/con_printer.py
+++
@@ -1,5 +0,0 @@
-import functions as func
-
-file_name = input(str('What is the file path for the votes file: '))
-print('The following is the consensus sequenece of the file.')
-print(func.get_consensus(file_name))
--- code/genome_modifier.py
+++
@@ -1,7 +0,0 @@
-import functions as func
-
-alignment_file = input('Type the path of the alignment file: ')
-
-
-func.alignment_sequences_modifier(alignment_file)
-
